ad4b7b64a1cf1dea83af03c3c6e16ce6a2026fa2
周军文 <junwen.zhou@tongdun.cn>
2020-08-12 22:54:35 +0800

    应用配置代码

--- dubbo-common/src/main/java/org/apache/dubbo/config/ApplicationConfig.java
+++ dubbo-common/src/main/java/org/apache/dubbo/config/ApplicationConfig.java
@@ -27,28 +27,16 @@ import org.apache.dubbo.config.support.Parameter;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-import static org.apache.dubbo.common.constants.CommonConstants.APPLICATION_KEY;
-import static org.apache.dubbo.common.constants.CommonConstants.DUMP_DIRECTORY;
-import static org.apache.dubbo.common.constants.CommonConstants.HOST_KEY;
-import static org.apache.dubbo.common.constants.CommonConstants.METADATA_KEY;
-import static org.apache.dubbo.common.constants.CommonConstants.SHUTDOWN_WAIT_KEY;
-import static org.apache.dubbo.common.constants.QosConstants.ACCEPT_FOREIGN_IP;
-import static org.apache.dubbo.common.constants.QosConstants.QOS_ENABLE;
-import static org.apache.dubbo.common.constants.QosConstants.QOS_HOST;
-import static org.apache.dubbo.common.constants.QosConstants.QOS_PORT;
-import static org.apache.dubbo.config.Constants.DEVELOPMENT_ENVIRONMENT;
-import static org.apache.dubbo.config.Constants.PRODUCTION_ENVIRONMENT;
-import static org.apache.dubbo.config.Constants.TEST_ENVIRONMENT;
+import static org.apache.dubbo.common.constants.CommonConstants.*;
+import static org.apache.dubbo.common.constants.QosConstants.*;
+import static org.apache.dubbo.config.Constants.*;
 
 
 /**
  * The application info
+ * 应用信息配置
  *
  * @export
  */
@@ -59,104 +47,158 @@ public class ApplicationConfig extends AbstractConfig {
 
     /**
      * Application name
+     * <p>
+     * 描述：当前应用名称，用于注册中心计算应用间依赖关系，
+     * 注意：消费者和提供者应用名不要一样，此参数不是匹配条件，
+     * 你当前项目叫什么名字就填什么，和提供者消费者角色无关，
+     * 比如：kylin应用调用了morgan应用的服务，则kylin项目配成kylin，
+     * morgan项目配成morgan，可能kylin也提供其它服务给别人使用，
+     * 但kylin项目永远配成kylin，这样注册中心将显示kylin依赖于morgan
+     * <p>
+     * 范围：服务治理
+     * <p>
+     * 是否必须：必填
      */
     private String name;
 
     /**
      * The application version
+     * <p>
+     * 描述：当前应用的版本
+     * <p>
+     * 范围：服务治理
+     * <p>
+     * 是否必须：否
      */
     private String version;
 
     /**
      * Application owner
+     * 描述： 应用负责人，用于服务治理，请填写负责人公司邮箱前缀
+     * <p>
+     * 范围： 服务治理
+     * <p>
+     * 是否必须：否
      */
     private String owner;
 
     /**
      * Application's organization (BU)
+     * 组织名称(BU或部门)，用于注册中心区分服务来源，此配置项建议不要使用autoconfig，
+     * 直接写死在配置中，比如china,intl,itu,crm,asc,dw,aliexpress等
      */
     private String organization;
 
     /**
      * Architecture layer
+     * 用于服务分层对应的架构。如，intl、china。不同的架构使用不同的分层。
      */
     private String architecture;
 
     /**
      * Environment, e.g. dev, test or production
+     * 应用环境，如：develop/test/product，从代码可以看到仅仅支持者三种
+     * 不同环境使用不同的缺省值，以及作为只用于开发测试功能的限制条件
      */
     private String environment;
 
     /**
      * Java compiler
+     * Java字节码编译器，用于动态类的生成，可选：jdk或javassist
      */
     private String compiler;
 
     /**
      * The type of the log access
+     * 日志输出方式，可选：slf4j,jcl,log4j,log4j2,jdk
      */
     private String logger;
 
     /**
      * Registry centers
+     * 注册中心
      */
     private List<RegistryConfig> registries;
+    /**
+     * 注册中心ID列表
+     */
     private String registryIds;
 
     /**
      * Monitor center
+     * 监控中心
      */
     private MonitorConfig monitor;
 
     /**
      * Is default or not
+     * 是否是默认
      */
     private Boolean isDefault;
 
     /**
      * Directory for saving thread dump
+     * 线程dump目录
      */
     private String dumpDirectory;
 
     /**
      * Whether to enable qos or not
+     * 是否开启qos
      */
     private Boolean qosEnable;
 
     /**
      * The qos host to listen
+     * qos监听主机地址
      */
     private String qosHost;
 
     /**
      * The qos port to listen
+     * qos监听端口
      */
     private Integer qosPort;
 
     /**
      * Should we accept foreign ip or not?
+     * 是否允许外网访问qos
      */
     private Boolean qosAcceptForeignIp;
 
     /**
      * Customized parameters
+     * 用户自定义参数
      */
     private Map<String, String> parameters;
 
     /**
      * Config the shutdown.wait
+     * shutdown等待时间，将结果赋值给了系统变量
+     * dubbo.service.shutdown.wait=shutwait
      */
     private String shutwait;
 
+    /**
+     * 主机名
+     */
     private String hostname;
 
     /**
      * Metadata type, local or remote, if choose remote, you need to further specify metadata center.
+     * 元数据类型，仅为 local或者remote
+     * 如果是remote，需要制定元数据中心
      */
     private String metadataType;
 
+    /**
+     * 是否注册为消费者
+     */
     private Boolean registerConsumer;
 
+    /**
+     * 仓库
+     */
     private String repository;
 
     public ApplicationConfig() {
@@ -166,6 +208,12 @@ public class ApplicationConfig extends AbstractConfig {
         setName(name);
     }
 
+    /**
+     * 生成的参数中的key为注解指定的key：application
+     * 但是其value却是从"dubbo.application.name"属性中获取的
+     *
+     * @return
+     */
     @Parameter(key = APPLICATION_KEY, required = true, useKeyAsProperty = false)
     public String getName() {
         return name;
@@ -178,6 +226,12 @@ public class ApplicationConfig extends AbstractConfig {
         }
     }
 
+    /**
+     * 生成的参数中的key为注解指定的key：application.version
+     * 但是其value却是从"dubbo.application.application.version"属性中获取的
+     *
+     * @return
+     */
     @Parameter(key = "application.version")
     public String getVersion() {
         return version;
@@ -344,7 +398,8 @@ public class ApplicationConfig extends AbstractConfig {
     }
 
     /**
-     * The format is the same as the springboot, including: getQosEnableCompatible(), getQosPortCompatible(), getQosAcceptForeignIpCompatible().
+     * The format is the same as the springboot, including: getQosEnableCompatible(), getQosPortCompatible(),
+     * getQosAcceptForeignIpCompatible().
      *
      * @return
      */
